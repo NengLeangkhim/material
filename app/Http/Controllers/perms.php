af43361cd49b844be0ee9eb7fb3fd5442a384412
Heng Seakkim <seakkim.heng@turbotechad.local>
2020-06-08 13:40:44 +0700

    sk->login&side bar

--- app/Http/Controllers/perms.php
+++ app/Http/Controllers/perms.php
@@ -9,46 +9,49 @@ use Illuminate\Support\Facades\DB;
 class perms extends Controller
 {
     //
-    public static function check_perm(){
+    private static function check_session(){
         if (session_status() == PHP_SESSION_NONE) {
             session_start();
         }
-        if(!empty($_SESSION['userid'])){
-            $mo=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",45)");
+        if(isset($_SESSION['userid'])){
+            if(!empty($_SESSION['userid'])){
+                return true;
+            }
+        }
+        header('Location:/logout');
+        exit;
+    }
+    public static function check_perm(){
+        if(self::check_session()){
+            $mo=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",null)");
             if(count($mo)>0){
                 return true;
             }else{
                 return false;
             }
         }else{
-            return view('login');
-            // return false;
+            // return view('login');
+            return false;
         }
     }
     public static function check_perm_module($mo){
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-        if(!empty($_SESSION['userid'])){
-            $mo=DB::select("SELECT public.exec_check_position($mo,".$_SESSION['userid'].") as id");
+        if(self::check_perm()){
+            $mo=DB::select("SELECT public.exec_check_position('$mo',".$_SESSION['userid'].") as id");
             if(!empty($mo[0]->id)){
                 return true;
             }else{
                 return false;
             }
         }else{
-            return view('login');
-            // return false;
+            // return view('no_perms');
+            return false;
         }
     }
     public static function get_module(){
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
         if(self::check_perm()){
-            $mo=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",45)");
+            $mo=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",null)");
             foreach($mo as $key=>$rr ){
-                $sub=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",".$rr->module_id.")");
+                $sub=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",'".$rr->code."')");
                 if(count($sub)>0){
                     $mo[$key]=array();
                     $mo[$key]= new \stdClass();
@@ -61,10 +64,11 @@ class perms extends Controller
                     $mo[$key]->child=false;
                 }
             }
-            usort($mo, function($a, $b) {
-                return $a->parent->sequence <=> $b->parent->sequence;
-            });
-            return $mo;
+            // usort($mo, function($a, $b) {
+            //     return $a->parent->sequence <=> $b->parent->sequence;
+            // });
+            // return $mo;
+            return self::output_module($mo);
         }else{
             return false;
         }
@@ -76,14 +80,13 @@ class perms extends Controller
         }
         if(!empty($_SESSION['userid'])){
             $mo=$module;
-            // $mo=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",$module)");
             if(count($mo)>0){
                 foreach($mo as $key=>$rr ){
-                    $sub=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",".$rr->module_id.")");
+                    $sub=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",'".$rr->code."')");
                     if(count($sub)>0){
                         $a[count($a)]=new \stdClass();
-                        $a[count($a)]->parent=$rr;
-                        $a[count($a)]->child=get_sub_modul($a,$sub);
+                        $a[count($a)-1]->parent=$rr;
+                        $a[count($a)-1]->child=$sub;//self::get_sub_modul($a,$sub);
                     }else{
                         $a[count($a)]=$rr;
                     }
@@ -94,10 +97,74 @@ class perms extends Controller
             }
         }
     }
-    private function check_sub($a,$sub){
+    private static function get_sub_module_of($module){//code
+        if(self::check_perm_module($module)){
+            $sub=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",'{$module}')");
+                return $sub;
+        }else{
+            return false;
+        }
+    }
+    private static function check_sub($a,$sub){
         session_start();
         if(!empty($_SESSION['userid'])){
             $mo=DB::select("SELECT * from public.exec_get_access_module_of(".$_SESSION['userid'].",$module)");
         }
+    }
+    private static function output_module($mo){
+        $st='';
+        foreach ($mo as $item){
+              $st.= "<li class='nav-item has-treevie'>";
+              $st.= "<a href='javascript:void(0);' class='nav-link active'>";
+              $st.= "<i class='nav-icon ".$item->parent->icon."'></i>";
+              $st.= " <p>";
+              $st.=  $item->parent->module_name;
+              if(($item->child))
+                {
+                  $st.= "<i class='right fas fa-angle-left'></i>";
+                }
+              $st.= "</p></a>";
+             if($item->child)
+                {
+                    $st.=self::output_sub($item->child,'');
+                }
+              $st.= " </li>  ";
+              }
+        return $st;
+    }
+    private static function output_sub($child,$flag){
+        $st='';
+        if($child){
+            if(is_array($child)){
+                foreach ($child as $rr) {
+                    if(isset($rr->parent)){
+                        $rr->parent->link=(empty($rr->parent->link))?'':"value='{$rr->parent->link}'";
+                        $st.= " <ul class='nav nav-treeview sub_menu'> ";
+                        $st.= "  <li class='nav-item has-treeview menu'  > ";
+                        $st.= "  <a href='javascript:void(0);' class='nav-link' {$rr->parent->link}  name='menu'> ";
+                        $st.= "  <i class='far fa-circle nav-icon'​></i> <i class='right fas fa-angle-left'></i>";
+                        $st.= "  <p>".$rr->parent->module_name."</p> </a>";
+                        $st.=self::output_sub($rr->child,'sub');
+                        $st.= "  </li></ul> ";
+                    }else{
+                        $sp=($flag=='')?'':'&nbsp&nbsp&nbsp&nbsp';
+                        $rr->link=(empty($rr->link))?'':"value='{$rr->link}'";
+                        $st.= " <ul class='nav nav-treeview sub_menu'> ";
+                        $st.= "  <li class='nav-item menu'  > ";
+                        $st.= "  <a href='javascript:void(0);' class='nav-link' ​$rr->link  name='menu'> ";
+                        $st.= "  $sp<i class='far fa-circle nav-icon'​></i> ";
+                        $st.= "  <p>$rr->module_name</p> ";
+                        $st.= "  </a></li></ul> ";
+                    }
+                }
+            }else{
+                foreach ($child as $rr) {
+                    if(isset($rr->link)){
+
+                    }
+                }
+            }
+        }
+        return $st;
     }
 }
