47145d6d9281932eaf83097018579897bb44d3e1
kopy <kopy9818@gmail.com>
2020-06-11 11:39:23 +0700

    v CRM2.0.0.0010

--- app/Http/Controllers/crm/ContactController.php
+++ app/Http/Controllers/crm/ContactController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\crm;
 
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\perms;
+use App\Http\Controllers\Controller;
 class ContactController extends Controller
 {
     public function getcontact(){
--- app/Http/Controllers/crm/OrganizationController.php
+++ app/Http/Controllers/crm/OrganizationController.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\crm;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\perms;
+use App\Http\Controllers\Controller;
 
 class OrganizationController extends Controller
 {
