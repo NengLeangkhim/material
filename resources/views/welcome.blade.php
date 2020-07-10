f7db1eb8a2e1efaabb7da524bf306c94cc148aac
Seakkim <seakkim78@gmail.com>
2020-07-10 11:23:05 +0700

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,26 +7,24 @@
                           <h3 class="card-title">{{ $dept[0]->name??'' }}</h3>
                         </div>
                         <!-- /.card-header -->
-                        <div class="row" style="padding: 1%;">
-                            <div class="col-md-3">
-                                <div class="card" style="padding: 3%;">
-                                    <img class="profile-pic" src="<?php echo $uself[0]->image??"/storage/avatar7.png";?>" style="width:100%" id='image_'>
-                                    <p>{{ $uself[0]->name??'' }}</p>
-                                    <p>{{ $uself[0]->position??'' }}</p>
-                                    <p>{{ $uself[0]->name??'' }}</p>
-                                    <p>{{ $uself[0]->phone??'' }}</p>
-                                </div>
-                            </div>
-                            <div class="col-md-6 welcome-bg">
-
-                            </div>
+                        <div class="row welcome-bg" style="padding: 1%;">
                             <div class="col-md-3 ">
                                 <div class="card" style="padding: 3%;">
                                     <img class="profile-pic" src="<?php echo $head[0]->image??"/storage/avatar7.png";?>" style="width:100%" id='image_'>
                                     <p>{{ $head[0]->name??'' }}</p>
                                     <p>{{ $head[0]->position??'' }}</p>
-                                    <p>{{ $head[0]->name??'' }}</p>
-                                    <p>{{ $head[0]->phone??'' }}</p>
+                                    <p>{{ $head[0]->contact??'' }}</p>
+                                    {{-- <p>{{ $head[0]->phone??'' }}</p> --}}
+                                </div>
+                            </div>
+                            <div class="col-md-6"></div>
+                            <div class="col-md-3">
+                                <div class="card" style="padding: 3%;">
+                                    <img class="profile-pic" src="<?php echo $uself[0]->image??"/storage/avatar7.png";?>" style="width:100%" id='image_'>
+                                    <p>{{ $uself[0]->name??'' }}</p>
+                                    <p>{{ $uself[0]->position??'' }}</p>
+                                    <p>{{ $uself[0]->contact??'' }}</p>
+                                    {{-- <p>{{ $uself[0]->contact??'' }}</p> --}}
                                 </div>
                             </div>
                         </div>
