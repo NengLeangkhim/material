94d57cf2787ddfb84685de45bae01abcea5f31c4
Kimsros2021 <62271902+Kimsros2021@users.noreply.github.com>
2020-07-30 07:58:29 +0700

    dv

--- resources/views/hrms/Employee/Attendance/AttendanceDetail.blade.php
+++ resources/views/hrms/Employee/Attendance/AttendanceDetail.blade.php
@@ -32,18 +32,62 @@
                                         <button class="btn bg-turbo-color form-control" >Search</button>
                                     </div>
                                 </div>
-                                <div class="col-md-12" style="margin-bottom: 10px">
+                                <div class="col-md-12" style="margin-bottom: 15px">
                                     <nav>
                                         <div class="nav nav-tabs" id="nav-tab" role="tablist">
                                             <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Monthly Attendance</a>
-                                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Late</a>
-                                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Absent</a>
+                                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Chart</a>
                                         </div>
                                     </nav>
                                     <div class="tab-content" id="nav-tabContent">
                                         <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
                                             <div class="" style="margin-top: 10px">
-                                                d
+                                                <table class="table table-bordered" id="tbl_overtime" style="width: 100%">
+                                                    <thead>                  
+                                                        <tr>
+                                                        <th style="width: 10px" rowspan="2">#</th>
+                                                        <th rowspan="2">Name</th>
+                                                        <th rowspan="2">Date</th>
+                                                        <th colspan="2">Morning</th></th>
+                                                        <th colspan="2">Evening</th>
+                                                        
+                                                        </tr>
+                                                        <tr>
+                                                        <th>Check In</th>
+                                                        <th>Check Out</th>
+                                                        <th>Check In</th>
+                                                        <th>Check Out</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                       
+                                                        <tr>
+                                                            <th>1</th>
+                                                            <td>Noun Sireyvunthy</td>
+                                                            <td>2020-07-17</td>
+                                                            <td>08:00:00</td>
+                                                            <td>12:05:00</td>
+                                                            <td>12:30:00</td>
+                                                            <td>17:35:00</td>
+                                                            
+                                                        </tr>
+                                                         <tr>
+                                                            <th>1</th>
+                                                            <td>Noun Sireyvunthy</td>
+                                                            <td>2020-07-17</td>
+                                                            <td>08:00:00</td>
+                                                            <td>12:05:00</td>
+                                                            <td>12:30:00</td>
+                                                            <td>17:35:00</td>
+                                                            
+                                                        </tr>
+                                                        
+                                                        
+                                                    
+                                                        
+                                                    </tbody>
+                                                </table>
+                                                
                                             </div>
                                         </div>
                                         <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
@@ -74,34 +118,7 @@
                                                 </tbody>
                                             </table>
                                         </div>
-                                        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                                            <table class="table" cellspacing="0">
-                                                <thead>
-                                                    <tr>
-                                                        <th>Contest Name</th>
-                                                        <th>Date</th>
-                                                        <th>Award Position</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td><a href="#">Work 1</a></td>
-                                                        <td>Doe</td>
-                                                        <td>john@example.com</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td><a href="#">Work 2</a></td>
-                                                        <td>Moe</td>
-                                                        <td>mary@example.com</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td><a href="#">Work 3</a></td>
-                                                        <td>Dooley</td>
-                                                        <td>july@example.com</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
+                                        
                                     </div>
                                 </div>
                             </div>
