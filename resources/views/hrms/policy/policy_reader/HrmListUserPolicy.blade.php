ce567a499ab773931fd1e81630abeafaf6136cc8
Metkeosambo <55055063+Metkeosambo@users.noreply.github.com>
2020-08-10 17:32:01 +0700

    kk
    kk

--- resources/views/hrms/policy/policy_reader/HrmListUserPolicy.blade.php
+++ resources/views/hrms/policy/policy_reader/HrmListUserPolicy.blade.php
@@ -5,7 +5,7 @@
          <div class="col-md-12">
              <div class="card">
                <div class="card-header">
-                 <h1 class="card-title hrm-title"><strong><i class="fas fa-book-open"></i> Policies</strong></h1>
+                 <h1 class="card-title hrm-title"><strong><i class="fas fa-book-open"></i> List Users Read Policy</strong></h1>
                  <div class="col-md-12 text-right">
 
                  </div>
