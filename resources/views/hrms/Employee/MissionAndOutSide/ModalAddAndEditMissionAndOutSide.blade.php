2d0d2420b3048734e79aaa7e9e4c9d35b7d047b6
Kimsros2021 <62271902+Kimsros2021@users.noreply.github.com>
2020-09-02 08:36:50 +0700

    df

--- resources/views/hrms/Employee/MissionAndOutSide/ModalAddAndEditMissionAndOutSide.blade.php
+++ resources/views/hrms/Employee/MissionAndOutSide/ModalAddAndEditMissionAndOutSide.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="card card-default">
           <div class="card-header">
-            <h3 class="card-title hrm-title"><strong><i class="fas fa-calendar-plus"></i> Add Mission or Outside</strong></h3>
+            <h3 class="card-title hrm-title"><strong><i class="fas fa-calendar-plus"></i> Add Mission or Onside</strong></h3>
 
             <div class="card-tools">
               <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
