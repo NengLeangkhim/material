3958be58b8fad24221fcd6959a5ecf1fbd0980bc
Metkeosambo <55055063+Metkeosambo@users.noreply.github.com>
2020-09-07 15:31:12 +0700

    ok
    ok

--- resources/views/crm/contact/index.blade.php
+++ resources/views/crm/contact/index.blade.php
@@ -4,7 +4,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Contact</h1>
+                            <h1><i class="fas fa-address-card"></i> <span>Contact</span></h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
