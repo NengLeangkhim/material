65e794bd219626ed1af453788f9c1228237be294
Kimsros2021 <62271902+Kimsros2021@users.noreply.github.com>
2020-09-14 15:59:21 +0700

    User Profile

--- app/Http/Controllers/hrms/Employee/AllemployeeController.php
+++ app/Http/Controllers/hrms/Employee/AllemployeeController.php
@@ -89,7 +89,7 @@ class AllemployeeController extends Controller
         $imageDirectory=$_POST['imgdirectory'];
         if($id>0){
            if(strlen($profile['name'])>0){
-                $upload=path_config::Move_Upload($profile,"media/hrms/Training/");
+                $upload=path_config::Move_Upload($profile,"/media/hrms/Training/");
                 if($upload==0){
                     $imageDirectory=$upload;
                     $em=Employee::UpdateEmployee($id,$firstName_en,$lastName_en,$email,$telephone,$position,8,16,$departement_id,$userid,$idNumber,$sex,$firstName_kh,$lastName_kh,$imageDirectory,$officePhone,$jointDate,$dateOfBirth,$homeNumber_en,$homeNumber_kh,$street_en,$street_kh,'null',$vilage,'null',$spous,$has_children,$chidren,$salary,4,$description,$bankaccount);
@@ -103,7 +103,7 @@ class AllemployeeController extends Controller
                 return;
            }
         }else{
-            $upload = path_config::Move_Upload($profile, "media/hrms/Training/");
+            $upload = path_config::Move_Upload($profile, "/media/hrms/Training/");
             if (!$upload == 0) {
                 $imageDirectory = $upload;
                 $em=Employee::InsertEmployee($firstName_en,$lastName_kh,$email,$telephone,$position,8,16,$departement_id,$userid,$idNumber,$sex,$firstName_kh,$lastName_kh,$imageDirectory,$officePhone,$jointDate,$dateOfBirth,$homeNumber_en,$homeNumber_kh,$street_en,$street_kh,'null', $vilage,'null',$spous,$has_children,$chidren,$salary,4,$description,$bankaccount);
--- app/Http/Controllers/path_config.php
+++ app/Http/Controllers/path_config.php
@@ -22,33 +22,12 @@ class path_config extends Controller
         $st=md5(time().$st).'.'.$extension;
         return $st;
     }
-    public static function movefile($file,$path){
-        $url_path = getcwd();
-        if ($file["error"] == 4) {
-            return 'error';
-        } else {
-            $file_path = $path . path_config::img_en(basename($file["name"]));
-            $p = $url_path . $path;
-            // $file_path=str_replace("'","''",$file_path);
-            if (move_uploaded_file($file["tmp_name"], $p)) {
-               return $file_path;
-            } else {
-                return 'fail';
-            }
-        }
-    }
-
-    public static function Move_File($file){
-        $filepdf = $file->getClientOriginalName(); // GET File name
-        $destinationPath = public_path('/media/hrms/file/'); //path for move
-        $filemove = $file->move($destinationPath, $filepdf); // move file to directory
-    }
-
     public static function Move_Upload($fileMove,$path){
         $filename = $fileMove['name'];
         $file = $fileMove['tmp_name'];
+        $url_path = getcwd();
         $renamefile= path_config::img_en(basename($filename));
-        $uploadfile = public_path($path).$renamefile;
+        $uploadfile = $url_path.$path.$renamefile;
         $filedirectory = $path.$renamefile;
         if (move_uploaded_file($file, $uploadfile)) {
             return $filedirectory;
