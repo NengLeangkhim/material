2c4a619fd5452155a463c150ba66eebef6d31633
Kimsros2021 <62271902+Kimsros2021@users.noreply.github.com>
2020-09-29 14:41:54 +0700

    testing

--- resources/views/hrms/Payroll/PayrollList.blade.php
+++ resources/views/hrms/Payroll/PayrollList.blade.php
@@ -80,7 +80,7 @@
                               }
                           @endphp
                           <button id="btn_{{$empayroll[0]}}" {{$disable}} style="margin-right:15px;border:none;background-color:none;color:#007bff;" href="javascrip:;" class="btn" onclick="DeleteComponent({{$empayroll[0]}},'{{$empayroll[7]}}','{{$empayroll[8]}}',{{$empayroll[9]}},'HRM_09040401')"><i class="far fa-trash-alt" ></i></button>
-                          <a style="margin-right:15px" href="javascrip:;" onclick="HRM_ShowDetail('hrm_paroll_detail','modal_payrolldetail',{{$empayroll[0]}})"><i class="fas fa-info"></i></a>
+                          <a style="margin-right:15px" href="javascript:;" onclick="HRM_ShowDetail('hrm_paroll_detail','modal_payrolldetail',{{$empayroll[0]}})"><i class="fas fa-info"></i></a>
                             <button id="btn_approve_{{$empayroll[0]}}" {{$disable}} class="btn {{$bg}} btn-sm" onclick="HR_Approve_Payroll({{$empayroll[0]}},'{{$empayroll[7]}}','{{$empayroll[8]}}',{{$empayroll[9]}},'btn_approve_{{$empayroll[0]}}','btn_{{$empayroll[0]}}','HRM_09040403')">Approved</button>
                         </td>
                     </tr>
