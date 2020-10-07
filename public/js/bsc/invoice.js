1708961f318e3564294dce4272bdfc1406afe0f0
Sok Seng <57388277+sokseng@users.noreply.github.com>
2020-10-07 13:58:32 +0700

    Update form invoice list

--- public/js/bsc/invoice.js
+++ public/js/bsc/invoice.js
@@ -1,25 +1,62 @@
-// Add and remove new record in invoice list
 $(document).ready(function(){
     var count = 1;
-    $('#add').click(function(){
-    count = count + 1;
-    var html_code = "<tr id='row"+count+"'>";
-    html_code += "<td><input type='text'​ name='item' class='form-control border_table'></td>";
-    html_code += "<td><input type='text'​ name='descript' class='form-control border_table'></td>";
-    html_code += "<td><input type='text'​ name='quantity' class='form-control border_table'></td>";
-    html_code += "<td><input type='text'​ name='unit_price' class='form-control border_table'></td>";
-    html_code += "<td><input type='text'​ name='discount' class='form-control border_table'></td>";
-    html_code += "<td><input type='text'​ name='account' class='form-control border_table'></td>";
-    html_code += "<td><input type='text'​ name='tax_rate' class='form-control border_table'></td>";
-    html_code += "<td><input type='text'​ name='amount' class='form-control border_table'></td>";
-    html_code += "<td id='remove_center'><button type='button' name='remove' data-row='row"+count+"' class='btn btn-danger btn-xs remove'><i class='fa fa-times'></i></button></td>";
-    html_code += "</tr>";
-    $('#crud_table').append(html_code);
+    $('.select2').select2();
+    // Loop to Display 4 Table
+    for(count=0;count<4;count++){
+        inSertTable(count);
+    }
+    // Insert Table
+    $('#invoice_form').click(function(){
+            inSertTable(count);
+            count = count + 1;
     });
+
+    // Remove Table
+    $(document).on('click', '.remove', function(){
+        var delete_row = $(this).data("row");
+        if($(this).closest('tbody').find('tr').length >1){
+            $('#' + delete_row).remove();
+            showTotal();
+        }
+    });
+
+    $("#invoice_table tbody").delegate('.item_qty','keyup',function(){
+        var qty=$(this).text();
+
+
+    });
+    $("#invoice_table tbody").delegate('.item_unit_price','keyup',function(){
+        tr=$(this).closest('tr');
+        var price=$(this).text();
+        var amount = price * tr.find('.item_qty').text();
+        tr.find('.item_amount').text(amount);
+        showTotal();
+    });
+
 });
 
-$(document).on('click', '.remove', function(){
-    var delete_row = $(this).data("row");
-    $('#' + delete_row).remove();
-});
-//End add and remove new record in invoice list
+function showTotal(){
+    let total_amount = 0;
+    $('.item_amount').each(function(e){
+        if(!isNaN(parseInt($(this).text()))){
+            total_amount += parseInt($(this).text());
+        }
+    });
+    document.getElementById('txtTotal').innerHTML=total_amount;
+}
+
+function inSertTable(count){
+    var tr = '';
+    tr +='<tr id="row'+count+'">'+
+            '<td contenteditable="true" id="txt_name" class="item_name" data-id="'+count+'"></td>'+
+            '<td contenteditable="true" class="item_des"></td>'+
+            '<td contenteditable="true" class="item_qty"></td>'+
+            '<td contenteditable="true" class="item_unit_price"></td>'+
+            '<td contenteditable="true" class="item_account"></td>'+
+            '<td contenteditable="true" class="item_tax"></td>'+
+            '<td class="tax_rate" id="tax_rate"></td>'+
+            '<td class="item_amount" id="item_amount"></td>'+
+            '<td style="text-align: center;"><button type="button" name="remove" data-row="row'+count+'" class="btn btn-danger btn-xs remove">x</button></td>'+
+        '</tr>';
+    $('#invoice_table tbody').append(tr);
+}
