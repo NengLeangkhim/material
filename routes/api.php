9dd8e99a36e476c80126abba6a7a43d4d114a610
ThinVytou <52662639+ThinVytou@users.noreply.github.com>
2020-10-07 08:35:39 +0700

    Update api show company

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,7 @@ Route::group(['middleware' => ['jwt.verify']], function() {
 // Chart account
 Route::resource('bsc_chart_accounts', 'api\BSC\ChartAccountController');
 Route::get('bsc_show_account_type', 'api\BSC\ChartAccountController@show_account_type');
+Route::get('bsc_show_company', 'api\BSC\ChartAccountController@show_company');
 Route::resource('bsc_purchases', 'api\BSC\PurchaseController');
 
 //======================END BSC=========================
