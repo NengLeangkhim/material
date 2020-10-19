8ed5311abf36bbf22b6ae4433f28bd29012eb43f
Metkeosambo <55055063+Metkeosambo@users.noreply.github.com>
2020-10-19 17:14:30 +0700

    CRM
    Update Contact

--- app/Http/Controllers/crm/ContactController.php
+++ app/Http/Controllers/crm/ContactController.php
@@ -16,12 +16,17 @@ class ContactController extends Controller
             session_start();
         }
         if(perms::check_perm_module('CRM_0205')){//module codes
-            //$contact_table=ModelCrmContact::CrmContactGetData(); // get data from model contact for table
+            $token = $_SESSION['token'];
             $request_contact = Request::create('/api/contacts', 'GET');
-            $contact_table = json_decode(Route::dispatch($request_contact)->getContent());
-           // $contact_pagination=ModelCrmContact::CrmContactGetDataPagination();  // get data from model contact for pagination
-            $request_pagination = Request::create('/api/contacts', 'GET');
-            $contact_pagination = json_decode(Route::dispatch($request_pagination)->getContent());
+            //$contact_table = json_decode(Route::dispatch($request_contact)->getContent());
+            $request_contact->headers->set('Accept', 'application/json');
+            $request_contact->headers->set('Authorization', 'Bearer '.$token);
+            $res = app()->handle($request_contact);
+            $contact_table = json_decode($res->getContent());
+            $contact_pagination=ModelCrmContact::CrmContactGetDataPagination();  // get data from model contact for pagination
+            // $request_pagination = Request::create('/api/contacts', 'GET');
+            // $contact_pagination = json_decode(Route::dispatch($request_pagination)->getContent());
+            // dd($contact_table);
             return view('crm.contact.index',['contact_table'=>$contact_table,'contact_pagination'=>$contact_pagination]); 
         }else{
             return view('no_perms');
@@ -31,9 +36,7 @@ class ContactController extends Controller
     public function FetchDataContact(Request $request) // function Get for paginaion contact
     {
         if ($request->ajax()){
-           // $contact_pagination=ModelCrmContact::CrmContactGetDataPagination(); // get data from model contact for pagination
-            $request_pagination = Request::create('/api/contacts', 'GET');
-            $contact_pagination = json_decode(Route::dispatch($request_pagination)->getContent());
+            $contact_pagination=ModelCrmContact::CrmContactGetDataPagination(); // get data from model contact for pagination
             return view('crm.contact.CrmPaginationContact',compact('contact_pagination'))->render();
         }
     }
