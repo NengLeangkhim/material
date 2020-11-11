bb7f13958657e55a5f5ddda423a260d9f1121a8e
Sok Seng <57388277+sokseng@users.noreply.github.com>
2020-11-11 17:01:34 +0700

    update view invoice

--- resources/views/bsc/chart_account/chart_account_list_edit.blade.php
+++ resources/views/bsc/chart_account/chart_account_list_edit.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1><span><i class="fas fa-user-edit"></i></span>Edit Chart Account</h1>
+                <h4><span><i class="fas fa-user-edit"></i></span>Edit Chart Account</h4>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
