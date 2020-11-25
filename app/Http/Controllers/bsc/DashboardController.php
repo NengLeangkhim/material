117a56dd8a3397eb689edb608c80fda02d249ef2
touch <touchrith096@gmail.com>
2020-11-25 16:02:05 +0700

    set permission bsc

--- app/Http/Controllers/bsc/DashboardController.php
+++ app/Http/Controllers/bsc/DashboardController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\bsc;
 
 use App\Http\Controllers\Controller;
 use Exception;
+use App\Http\Controllers\perms;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function list()
-    {
+    {   
+        if(!perms::check_perm_module('BSC_0306')){
+            return view('no_perms');
+        }
         try{
             if (session_status() == PHP_SESSION_NONE) {
                 session_start();
@@ -30,25 +34,5 @@ class DashboardController extends Controller
             exit();
         }
 
-    }
-
-    public static function read_data()
-    {
-        try{
-            if (session_status() == PHP_SESSION_NONE) {
-                session_start();
-            }
-            $token = $_SESSION['token'];
-             //get dashboard data
-             $request = Request::create('/api/bsc_dashboards', 'GET');
-             $request->headers->set('Accept', 'application/json');
-             $request->headers->set('Authorization', 'Bearer '.$token);
-             $res = app()->handle($request);
-             $ch_account = json_decode($res->getContent()); // convert to json object
-             $ch_accounts=$ch_account->data;
-        }catch(Exception $e){
-            echo $e->getMessage();
-            exit();
-        }
     }
 }
