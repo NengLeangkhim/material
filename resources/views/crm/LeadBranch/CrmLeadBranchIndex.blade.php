8faeecf6da96d15c5337e763b234b9a0247489a7
Thol2019 <50733400+Thol2019@users.noreply.github.com>
2020-12-02 17:21:57 +0700

    add new feature Lead Branch

--- resources/views/crm/LeadBranch/CrmLeadBranchIndex.blade.php
+++ resources/views/crm/LeadBranch/CrmLeadBranchIndex.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1><i class="fas fa-cogs"></i> Leads Branch</h1>
+                <h1><i class="fas fa-user"></i> Leads Branch</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
