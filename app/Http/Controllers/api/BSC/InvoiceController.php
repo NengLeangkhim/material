8b09a6d719b4d89a067de1348cb3fc46ba749cb3
ThinVytou <52662639+ThinVytou@users.noreply.github.com>
2020-12-09 09:38:43 +0700

    Fix bug balancesheet BSC

--- app/Http/Controllers/api/BSC/InvoiceController.php
+++ app/Http/Controllers/api/BSC/InvoiceController.php
@@ -380,7 +380,7 @@ class InvoiceController extends Controller
                         LEFT JOIN ma_customer ON crm_quote.crm_lead_id = ma_customer.crm_lead_id
                         LEFT JOIN bsc_invoice ON crm_quote.id = bsc_invoice.crm_quote_id
                     WHERE
-                        qs.crm_quote_status_type_id = 2 
+                        qs.crm_quote_status_type_id = 16 
                         AND ma_customer.id IS NOT null  
                         AND bsc_invoice.crm_quote_id IS NULL
                         AND crm_quote.status = 't' 
