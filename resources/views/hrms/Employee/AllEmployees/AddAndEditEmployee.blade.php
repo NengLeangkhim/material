8fb4f571de4d1ab39e9e4446498f0e2a0222b1a9
Kimsros2021 <62271902+Kimsros2021@users.noreply.github.com>
2020-12-17 15:39:13 +0700

    ss

--- resources/views/hrms/Employee/AllEmployees/AddAndEditEmployee.blade.php
+++ resources/views/hrms/Employee/AllEmployees/AddAndEditEmployee.blade.php
@@ -1,3 +1,30 @@
+<div id="uploadimageModal" class="modal" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+      		<div class="modal-header">
+        		<button type="button" class="close" data-dismiss="modal">&times;</button>
+        		<h4 class="modal-title">Upload & Crop Image</h4>
+      		</div>
+      		<div class="modal-body">
+        		<div class="row">
+  					<div class="col-md-8 text-center">
+						  <div id="image_demo" style="width:350px; margin-top:30px"></div>
+  					</div>
+  					<div class="col-md-4" style="padding-top:30px;">
+  						<br />
+  						<br />
+  						<br/>
+						  <button class="btn btn-success crop_image">Crop & Upload Image</button>
+					</div>
+				</div>
+      		</div>
+      		<div class="modal-footer">
+        		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      		</div>
+    	</div>
+    </div>
+</div>
+
 
 <div class="modal fade show" id="modal_employee" style="display: block; padding-right: 17px;" aria-modal="true" data-backdrop="static">
     <div class="modal-dialog modal-xl">
@@ -208,6 +235,19 @@
                   <img id="output_image" name="emProfile" height="300px" width="250px" src="@php if(isset($data[1])){ echo $data[1]['image'];} @endphp"/>
                 </div>
             </div>
+
+            {{-- <div class="col-md-3 text-center border">
+                <div class="col-md-12 text-center" style="width: 100%">
+                    <div style="width:250px;height:300px" class="border text-center" id="uploaded_image">
+                        <img id="output_image" name="emProfile" height="300px" width="250px" src="@php if(isset($data[1])){ echo $data[1]['image'];} @endphp"/>
+                    </div>
+                    <br>
+                    <input type="file" name="emProfile" id="upload_image" onchange="preview_image(event)">
+                </div>
+
+
+                
+            </div> --}}
 
             
             
@@ -986,3 +1026,52 @@
         </div>
     </div>
 </div>
+<script>  
+$(document).ready(function(){
+
+	$image_crop = $('#image_demo').croppie({
+    enableExif: true,
+    viewport: {
+      width:200,
+      height:200,
+      type:'square' //circle
+    },
+    boundary:{
+      width:300,
+      height:300
+    }
+  });
+
+  $('#upload_image').on('change', function(){
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      $image_crop.croppie('bind', {
+        url: event.target.result
+      }).then(function(){
+        console.log('jQuery bind complete');
+      });
+    }
+    reader.readAsDataURL(this.files[0]);
+    $('#uploadimageModal').modal('show');
+  });
+
+  $('.crop_image').click(function(event){
+    $image_crop.croppie('result', {
+      type: 'canvas',
+      size: 'viewport'
+    }).then(function(response){
+      $.ajax({
+        url:"upload.php",
+        type: "POST",
+        data:{"image": response},
+        success:function(data)
+        {
+          $('#uploadimageModal').modal('hide');
+          $('#uploaded_image').html(data);
+        }
+      });
+    })
+  });
+
+});  
+</script>
